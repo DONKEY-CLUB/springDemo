ed37eded123b27b7c81a6ecb345ea9f28ed06372
01Time03Skipper02 <sasha.shvech2003@mail.ru>
2022-07-08 22:16:31 +0300

    #2
    #2

--- src/main/java/com/example/springdemo/controllers/MainController.java
+++ src/main/java/com/example/springdemo/controllers/MainController.java
@@ -3,7 +3,6 @@ package com.example.springdemo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,7 +10,7 @@ public class MainController {
     @GetMapping("/")
     public String home( Model model) {
         model.addAttribute("title", "Главная страница");
-        return "home";
+        return "index";
     }
 
 }
