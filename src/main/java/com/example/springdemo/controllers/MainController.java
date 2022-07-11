6ad47ecad075682cb7b5544dfb7e48e276b3173f
01Time03Skipper02 <sasha.shvech2003@mail.ru>
2022-07-11 13:14:41 +0300

    Fix styles
    Finally add fixed styles

--- src/main/java/com/example/springdemo/controllers/MainController.java
+++ src/main/java/com/example/springdemo/controllers/MainController.java
@@ -13,4 +13,50 @@ public class MainController {
         return "index";
     }
 
+    //ADD STYLES
+    @GetMapping("/css/style.css")
+    public String homeStyle1(Model model){
+        return "/css/style.css";
+    }
+
+    @GetMapping("/css/animate.css")
+    public String homeStyle2(Model model){
+        return "/css/animate.css";
+    }
+
+    @GetMapping("/font-awesome/css/font-awesome.css")
+    public String homeStyle3(Model model){
+        return "/font-awesome/css/font-awesome.css";
+    }
+
+    @GetMapping("/css/bootstrap.min.css")
+    public String homeStyle4(Model model){
+        return "/css/bootstrap.min.css";
+    }
+
+    //ADD SCRIPTS
+    @GetMapping("/js/jquery-2.1.1.js")
+    public String homeScript1( Model model) {
+        return "/js/jquery-2.1.1.js";
+    }
+    @GetMapping("/js/bootstrap.min.js")
+    public String homeScript2( Model model) {
+        return "/js/bootstrap.min.js";
+    }
+    @GetMapping("/js/plugins/metisMenu/jquery.metisMenu.js")
+    public String homeScript3( Model model) {
+        return "/js/plugins/metisMenu/jquery.metisMenu.js";
+    }
+    @GetMapping("/js/plugins/slimscroll/jquery.slimscroll.min.js")
+    public String homeScript4( Model model) {
+        return "/js/plugins/slimscroll/jquery.slimscroll.min.js";
+    }
+    @GetMapping("/js/inspinia.js")
+    public String homeScript5( Model model) {
+        return "/js/inspinia.js";
+    }
+    @GetMapping("/js/plugins/pace/pace.min.js")
+    public String homeScript6( Model model) {
+        return "/js/plugins/pace/pace.min.js";
+    }
 }
