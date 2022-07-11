d6a04a4ae462458b9e2fe17f83a52b5d7d4fdfb2
01Time03Skipper02 <sasha.shvech2003@mail.ru>
2022-07-11 14:53:37 +0300

    Fix styles

--- src/main/java/com/example/springdemo/controllers/ScriptController.java
+++ src/main/java/com/example/springdemo/controllers/ScriptController.java
@@ -7,29 +7,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ScriptController {
 
-    @GetMapping("/js/jquery-2.1.1.js")
+    @GetMapping("/static/scripts/js/jquery-2.1.1.js")
     public String homeScript1( Model model) {
-        return "/js/jquery-2.1.1.js";
+        return "../static/scripts/js/jquery-2.1.1.js";
     }
-    @GetMapping("/js/bootstrap.min.js")
+    @GetMapping("/static/scripts/js/bootstrap.min.js")
     public String homeScript2( Model model) {
-        return "/js/bootstrap.min.js";
+        return "../static/scripts/js/bootstrap.min.js";
     }
-    @GetMapping("/js/plugins/metisMenu/jquery.metisMenu.js")
+    @GetMapping("/static/scripts/js/plugins/metisMenu/jquery.metisMenu.js")
     public String homeScript3( Model model) {
-        return "/js/plugins/metisMenu/jquery.metisMenu.js";
+        return "../static/scripts/js/plugins/metisMenu/jquery.metisMenu.js";
     }
-    @GetMapping("/js/plugins/slimscroll/jquery.slimscroll.min.js")
+    @GetMapping("/static/scripts/js/plugins/slimscroll/jquery.slimscroll.min.js")
     public String homeScript4( Model model) {
-        return "/js/plugins/slimscroll/jquery.slimscroll.min.js";
+        return "../static/scripts/js/plugins/slimscroll/jquery.slimscroll.min.js";
     }
-    @GetMapping("/js/inspinia.js")
+    @GetMapping("/static/scripts/js/inspinia.js")
     public String homeScript5( Model model) {
-        return "/js/inspinia.js";
+        return "../static/scripts/js/inspinia.js";
     }
-    @GetMapping("/js/plugins/pace/pace.min.js")
+    @GetMapping("/static/scripts/js/plugins/pace/pace.min.js")
     public String homeScript6( Model model) {
-        return "/js/plugins/pace/pace.min.js";
+        return "../static/scripts/js/plugins/pace/pace.min.js";
     }
-    
+
 }
--- src/main/java/com/example/springdemo/controllers/StyleController.java
+++ src/main/java/com/example/springdemo/controllers/StyleController.java
@@ -7,24 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StyleController {
 
-    @GetMapping("/css/style.css")
+    @GetMapping("/static/styles/css/style.css")
     public String homeStyle1(Model model){
-        return "/css/style.css";
+        return "../static/styles/css/style.css";
     }
 
-    @GetMapping("/css/animate.css")
+    @GetMapping("/static/styles/css/animate.css")
     public String homeStyle2(Model model){
-        return "/css/animate.css";
+        return "../static/styles/css/animate.css";
     }
 
-    @GetMapping("/font-awesome/css/font-awesome.css")
+    @GetMapping("/static/styles/font-awesome/css/font-awesome.css")
     public String homeStyle3(Model model){
-        return "/font-awesome/css/font-awesome.css";
+        return "../static/styles/font-awesome/css/font-awesome.css";
     }
 
-    @GetMapping("/css/bootstrap.min.css")
+    @GetMapping("/static/styles/css/bootstrap.min.css")
     public String homeStyle4(Model model){
-        return "/css/bootstrap.min.css";
+        return "../static/styles/css/bootstrap.min.css";
     }
 
 }
