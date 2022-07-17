5dbda4d07c97a66ae9b53b0122a54fa6354ffcec
01Time03Skipper02 <sasha.shvech2003@mail.ru>
2022-07-17 20:22:52 +0300

    Some changes of getting ip function

--- src/main/java/com/example/springdemo/controllers/MainController.java
+++ src/main/java/com/example/springdemo/controllers/MainController.java
@@ -1,18 +1,20 @@
 package com.example.springdemo.controllers;
 
+import com.example.springdemo.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.net.InetAddress;
 import java.util.*;
 import java.util.stream.Collectors;
 
 
 @Controller
 public class MainController {
-
+    //function of getting directory
     public static List<File> files(String dirname) {
         if (dirname == null) {
             return Collections.emptyList();
@@ -31,81 +33,70 @@ public class MainController {
                 .collect(Collectors.toList());
     }
 
+    @Autowired
+    private RequestService requestService;
+
     @GetMapping("/index.html")
-    public String index(Model model) {
+    public String index(HttpServletRequest request, Model model) {
+        //get Date
         Date date = new Date(System.currentTimeMillis());
+        //get IP
+        String clientIp = requestService.getClientIp(request);
 
         //Get all files from directory
-        List<File> files = files("C:\\Users\\1\\Desktop\\GitHub");
+        List<File> files = files("C:\\Users\\1\\Desktop\\GitHub"); //Later... Take user's directory
         int n = files.size();
         String[] directory = new String[n];
         for(int i = 0; i < n; i++){
             directory[i] = files.get(i).getName();
         }
 
-        //Try to get IP
-        try {
-            InetAddress clientIPAddress = InetAddress.getLocalHost();
-            model.addAttribute("clientFileSystem", directory);
-            model.addAttribute("clientIPAddress", clientIPAddress.getHostAddress());
-            model.addAttribute("clientDate", date);
-        } catch (Exception ex) {
-            model.addAttribute("clientFileSystem", directory);
-            model.addAttribute("clientDate", date);
-            System.out.println(ex.getMessage());
-        }
+        model.addAttribute("clientFileSystem", directory);
+        model.addAttribute("clientDate", date);
+        model.addAttribute("clientIPAddress", clientIp);
+
         return "index";
     }
 
     @GetMapping("/minor.html")
-    public String home2(Model model) {
+    public String home2(HttpServletRequest request, Model model) {
+        //get Date
         Date date = new Date(System.currentTimeMillis());
+        //get IP
+        String clientIp = requestService.getClientIp(request);
 
         //Get all files from directory
-        List<File> files = files("C:\\Users\\1\\Desktop\\GitHub");
+        List<File> files = files("C:\\Users\\1\\Desktop\\GitHub"); //Later... Take user's directory
         int n = files.size();
         String[] directory = new String[n];
         for(int i = 0; i < n; i++){
             directory[i] = files.get(i).getName();
         }
 
-        //Try to get IP
-        try {
-            InetAddress clientIPAddress = InetAddress.getLocalHost();
-            model.addAttribute("clientFileSystem", directory);
-            model.addAttribute("clientIPAddress", clientIPAddress.getHostAddress());
-            model.addAttribute("clientDate", date);
-        } catch (Exception ex) {
-            model.addAttribute("clientFileSystem", directory);
-            model.addAttribute("clientDate", date);
-            System.out.println(ex.getMessage());
-        }
+        model.addAttribute("clientFileSystem", directory);
+        model.addAttribute("clientDate", date);
+        model.addAttribute("clientIPAddress", clientIp);
         return "minor";
     }
 
     @GetMapping("/profiles.html")
-    public String profile(Model model) {
+    public String profile(HttpServletRequest request, Model model) {
+        //get Date
         Date date = new Date(System.currentTimeMillis());
+        //get IP
+        String clientIp = requestService.getClientIp(request);
 
         //Get all files from directory
-        List<File> files = files("C:\\Users\\1\\Desktop\\GitHub");
+        List<File> files = files("C:\\Users\\1\\Desktop\\GitHub"); //Later... Take user's directory
         int n = files.size();
         String[] directory = new String[n];
         for(int i = 0; i < n; i++){
             directory[i] = files.get(i).getName();
         }
 
-        //Try to get IP
-        try {
-            InetAddress clientIPAddress = InetAddress.getLocalHost();
-            model.addAttribute("clientFileSystem", directory);
-            model.addAttribute("clientIPAddress", clientIPAddress.getHostAddress());
-            model.addAttribute("clientDate", date);
-        } catch (Exception ex) {
-            model.addAttribute("clientFileSystem", directory);
-            model.addAttribute("clientDate", date);
-            System.out.println(ex.getMessage());
-        }
+        model.addAttribute("clientFileSystem", directory);
+        model.addAttribute("clientDate", date);
+        model.addAttribute("clientIPAddress", clientIp);
         return "profiles";
     }
 
@@ -124,7 +115,4 @@ public class MainController {
         return "register";
     }
 
-    public static void main (String[] args){
-
-    }
 }
